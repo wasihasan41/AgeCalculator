8d6dbe38f64bf70a5807901aefd06dd29ed7c4a4
wasihasan41 <wasihasan41@gmail.com>
2025-02-27 22:54:31 +0530

    continue...

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('.btn')
+const output = document.querySelector('.output')
 let dayInput = document.querySelector('.day-input');
 let monthInput = document.querySelector('.month-input');
 let yearInput = document.querySelector('.year-input') 
@@ -7,38 +8,31 @@ button.addEventListener('click', getDay)
 
 
 function getDay(){
-  let dateOfBirth = dayjs().set('year', dayInput.value).set('month', monthInput.value-1).set('year', yearInput.value)
-  console.log(dateOfBirth.format('YYYY-MM-DD'))
+  let dateOfBirth = dayjs().set('date', dayInput.value ).set('month', monthInput.value-1).set('year', yearInput.value)
+  if(dayInput.value || monthInput.value || yearInput.value == ''){
+    output.innerHTML = 'Enter full age'
+  }
+  if(dayInput.value || monthInput.value || yearInput.value == true){
+    output.innerHTML = 'and all set'
+  }
+  ageCalc(dateOfBirth)
 }
 
-const ageCalc = () => {
-  const currentDate = dayjs('2025-02-26');
-  const birthDate = dayjs('2004-02-11');
-  let years = currentDate.diff(birthDate, 'year')
-  console.log(years)
-  let birthYear = birthDate.add(years, 'year')
-  console.log(birthYear)
-  const months = currentDate.diff(birthYear, 'months')
-  console.log(months)
-  const birthMonth = birthYear.add(months, 'month')
-  console.log(birthMonth)
-  let days = currentDate.diff(birthMonth, 'days')
-  console.log(days)
-  let birthDay = birthMonth.add(days, 'days')
-  console.log(birthDay)
-  
+function ageCalc(dateOfBirth) {
+  const currentDate = dayjs();
+  const birthDate = dayjs(dateOfBirth);
+
+  let years = currentDate.diff(birthDate, 'year');
+  console.log(years + ' years');
+
+  let birthYear = birthDate.add(years, 'year');
+
+  const months = currentDate.diff(birthYear, 'months');
+  console.log(months + ' months');
+
+  const birthMonth = birthYear.add(months, 'month');
+
+  let days = currentDate.diff(birthMonth, 'days');
+  console.log(days + ' days');
 }
-// const currentDate = dayjs();
-// const birthDate = dayjs('2004-02-11');
-// let years = currentDate.diff(birthDate, 'year')
-// console.log(years)
-// let birthYear = birthDate.add(years, 'year')
-// console.log(birthYear)
-// const months = currentDate.diff(birthYear, 'months')
-// console.log(months)
-// const birthMonth = birthYear.add(months, 'month')
-// console.log(birthMonth)
-// let days = currentDate.diff(birthMonth, 'days')
-// console.log(days)
-// let birthDay = birthMonth.add(days, 'days')
-// console.log(birthDay)
+ ageCalc()
